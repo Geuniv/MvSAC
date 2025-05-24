959c22daa2ea7aa16b3213c2e142a7ee048d7396
jinwhan804 <jinwhan804@hanmail.net>
2025-05-24 17:08:22 +0900

    0524 change movein model and route, create movein reg jsx

---
+++ frontend/src/MoveIn/MoveInReg.jsx
@@ -0,0 +1,14 @@
+export default function MoveInReg () {
+    return(
+        <>
+            <form>
+                <input name="name" type="text"/>
+                <input name="rrn" type="text"/>
+                <input name="email" type="text"/>
+                <input name="beforeAddr" type="text"/>
+                <input name="afterAddr" type="text"/>
+                <input name="moveInDt" type="date"/>
+            </form>
+        </>
+    )
+}
