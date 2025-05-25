a60bdbd53f4de13b3f618a3299fe082c937d7736
jinwhan804 <jinwhan804@hanmail.net>
2025-05-25 16:58:30 +0900

    0525 install react route model, update of moveinreg, create moveinupdate

--- frontend/src/MoveIn/MoveInReg.jsx
+++ frontend/src/MoveIn/MoveInReg.jsx
@@ -1,13 +1,56 @@
+import axios from 'axios';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function MoveInReg () {
+    // const navigator = useNavigate();
+
+    const [ form, setForm ] = useState({
+        name: "",
+        rrn: "",
+        email: "",
+        beforeAddr: '',
+        afterAddr: '',
+        moveInDt: ''
+    });
+
+    const { name, rrn, email, beforeAddr, afterAddr, moveInDt } = form;
+
+    const handleChange = e => setForm({...form, [e.target.name]: e.target.value})
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        axios
+        .post("http://localhost:8000/movein/",
+            formData,
+            {
+                headers: {
+                    "Content-Type": "multipart/form-data"
+                }
+            }
+        )
+        .then(res => {
+            console.log(res);
+            if (res.status === 201) {
+                    alert(res.data.message);
+                    // navigator("/list");
+                }
+        })
+        .catch(err => {
+            console.log(err);
+            alert("전입 등록 실패")
+        })
+    }
     return(
         <>
-            <form>
-                <input name="name" type="text"/>
-                <input name="rrn" type="text"/>
-                <input name="email" type="text"/>
-                <input name="beforeAddr" type="text"/>
-                <input name="afterAddr" type="text"/>
-                <input name="moveInDt" type="date"/>
+            <form onSubmit={handleSubmit}>
+                <input onChange={handleChange} value={name} name="name" type="text" placeholder='신청자 이름 입력'/>
+                <input onChange={handleChange} value={rrn} name="rrn" type="number" placeholder='주민번호 입력'/>
+                <input onChange={handleChange} value={email} name="email" type="text" placeholder='이메일 입력'/>
+                <input onChange={handleChange} value={beforeAddr} name="beforeAddr" type="text" placeholder='입주 전 주소'/>
+                <input onChange={handleChange} value={afterAddr} name="afterAddr" type="text" placeholder='입주 후 주소'/>
+                <input onChange={handleChange} value={moveInDt} name="moveInDt" type="date" placeholder='입주 예정일'/>
+                <button type="submit">등록</button>
             </form>
         </>
     )
