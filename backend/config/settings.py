3ec43f322f634761a18c95edc59b833778f95e57
Geuniv <kkoowl99@gmail.com>
2025-05-28 16:13:47 +0900

    2025.05.28_settings, hash_rrn 수정

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -9,7 +9,7 @@ load_dotenv()  # .env 파일 읽기
 class Settings(BaseSettings):
     database_url: str = os.getenv("DATABASE_URL")
     secret_key: str = os.getenv("SECRET_KEY")
-    # rrn_secret_key: str = os.getenv("RRN_SECRET_KEY")
+    rrn_secret_key: str = os.getenv("RRN_SECRET_KEY")
 
     # Naver Cloud Platform 설정
     # ncp_access_key: str = os.getenv("NCP_ACCESS_KEY")
